d2c3653b72eb4a9c0ed581d1e6078925158526c7
Maud <maud.becam@epsi.fr>
2021-05-20 18:17:55 +0200

    Création du projet Java avec Spring Initializr

---
+++ src/main/java/fr/epsi/mspr/MsprApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.mspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/mspr/MsprApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.mspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
