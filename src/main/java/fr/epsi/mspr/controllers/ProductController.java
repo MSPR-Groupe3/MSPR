81d89af3eaaa6d0012fdd5aae196c649128ceda3
Maud <maud.becam@epsi.fr>
2021-05-26 16:13:58 +0200

    Création du package pour les controllers

--- src/main/java/fr/epsi/mspr/controllers/ProductController.java
+++ src/main/java/fr/epsi/mspr/controllers/ProductController.java
@@ -1,4 +1,15 @@
 package fr.epsi.mspr.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ProductController {
+
+    //private ProductRepository;
+
+    @GetMapping("/indexProducts")
+    public String showProductList(Model model){
+
+        return null;
+    }
 }
