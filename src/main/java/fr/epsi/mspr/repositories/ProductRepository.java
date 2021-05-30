bec5048ff5f08f7099c68b1972b75b2fb2a7126e
Maud <maud.becam@epsi.fr>
2021-05-30 15:35:39 +0200

    Ajout des annotations pour les classes Repositories

--- src/main/java/fr/epsi/mspr/repositories/ProductRepository.java
+++ src/main/java/fr/epsi/mspr/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package fr.epsi.mspr.repositories;
 
 import fr.epsi.mspr.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/fr/epsi/mspr/repositories/UserRepository.java
+++ src/main/java/fr/epsi/mspr/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package fr.epsi.mspr.repositories;
 
 import fr.epsi.mspr.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
