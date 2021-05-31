7808eb2c3a2ed75933d927eb9b6a85db128501d5
philaden-nd <tranngocphi.h@gmail.com>
2021-05-31 21:43:36 +0200

    clean SQL columns naming and add foreign keys

--- src/main/resources/SQL_scripts/creation.sql
+++ src/main/resources/SQL_scripts/creation.sql
@@ -2,65 +2,74 @@ DROP DATABASE IF EXISTS mspr;
 CREATE DATABASE mspr CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 USE mspr;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
+SET time_zone = '+00:00';
 
 CREATE TABLE IF NOT EXISTS `user` (
     `id` bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `firstName` varchar(255),
-    `lastName` varchar(255),
-    `loginEmail` varchar(255),
+    `first_name` varchar(255),
+    `last_name` varchar(255),
+    `login_email` varchar(255),
     `passwd` varchar(255),
     `role` varchar(255)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `organization` (
     `id` bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `companyName` varchar(255),
-    `streetAdress1` varchar(255),
-    `streetAdress2` varchar(255),
-    `streetAdress3` varchar(255),
-    `cityName` varchar(255),
-    `postalCode` varchar(255),
+    `company_name` varchar(255) UNIQUE,
+    `street_address1` varchar(255),
+    `street_address2` varchar(255),
+    `street_address3` varchar(255),
+    `city_name` varchar(255),
+    `postal_code` varchar(255),
     `country` varchar(255),
-    `phoneNumber` varchar(255),
+    `phone_number` varchar(255),
     `email` varchar(255)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `contact` (
     `id` bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `firstName` varchar(255),
-    `lastName` varchar(255),
-    `phoneNumber` varchar(255),
-    `email` varchar(255)
+    `first_name` varchar(255),
+    `last_name` varchar(255),
+    `phone_number` varchar(255),
+    `email` varchar(255),
+    `organization_id` bigint(20),
+    CONSTRAINT fk_organization FOREIGN KEY (`organization_id`)
+    REFERENCES `organization`(`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `purchase` (
     `id` bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `reference` varchar(255),
-    `dateOfOrder` datetime,
+    `reference` varchar(255) UNIQUE,
+    `date_of_order` datetime,
     `comment` varchar(255)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `product` (
     `id` bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` varchar(255),
-    `reference` varchar(255),
+    `reference` varchar(255) UNIQUE,
     `description` varchar(255),
-    `unitPriceBeforeTax` float,
-    `taxRate` float,
-    `quantityAvailable` int,
-    `isSellable` boolean
+    `unit_price_before_tax` float,
+    `tax_rate` float,
+    `quantity_available` int,
+    `is_sellable` boolean
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `product_in_purchase` (
+    `purchase_id` bigint(20),
+    `product_id` bigint(20),
     `quantity` int(20) NOT NULL,
     `price` float NOT NULL,
-    PRIMARY KEY (`quantity`, `price`)
+    PRIMARY KEY (`quantity`, `price`),
+    CONSTRAINT fk_purchase FOREIGN KEY (`purchase_id`)
+    REFERENCES `purchase`(`id`),
+    CONSTRAINT fk_product FOREIGN KEY (`product_id`)
+    REFERENCES `product`(`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `category` (
     `id` bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name` varchar(255)
+    `name` varchar(255) UNIQUE
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- src/main/resources/SQL_scripts/filling.sql
+++ src/main/resources/SQL_scripts/filling.sql
@@ -1,55 +1,55 @@
 USE mspr;
 
-INSERT INTO `user` (`firstName`, `lastName`, `loginEmail`, `passwd`, `role`) VALUES
-("Maud","Becam","maudus@aol.fr", "$2y$10$aZ4dOMDeVBZdMEEtbrE1UeB.uGgNbVMOgkF4blMaFssdUm5zPgTBS", "seller"),
-("Mickaëlle","Green","mi-k@skyblog.fr","$2y$10$PlfSGdi0KNPTd56.C2Mkk.Am4dLDdtrE0MFyt/Pm8ZE9ANQRsqJgm", "seller"),
-("Phi","Tran","filoo@msn.com","$2y$10$BfMtK5f53pEBLk7h0qTDe.MY.tjijMFB4ZWLMv0MdUsv.x3SNwhny", "seller"),
-("Corentin","Villiermet","coco@wanadoo.fr","$2y$10$ZPbU3HJ5gQ3/HFh1xWXBYuybb6ZEXulAzm9Cww/5cK88WdNiBFdom", "seller"),
-("Mickaël","Gaillard","micky@lycoos.com","$2y$10$7UILhfy1byfq8rSdnnXkuOxfB.29Y/yKtHgc5DH9D4M3iy5Ccr41K", "admin");
+INSERT INTO `user` (`first_name`, `last_name`, `login_email`, `passwd`, `role`) VALUES
+('Maud','Becam','maudus@aol.fr', '$2y$10$aZ4dOMDeVBZdMEEtbrE1UeB.uGgNbVMOgkF4blMaFssdUm5zPgTBS', 'seller'),
+('Mickaëlle','Green','mi-k@skyblog.fr','$2y$10$PlfSGdi0KNPTd56.C2Mkk.Am4dLDdtrE0MFyt/Pm8ZE9ANQRsqJgm', 'seller'),
+('Phi','Tran','filoo@msn.com','$2y$10$BfMtK5f53pEBLk7h0qTDe.MY.tjijMFB4ZWLMv0MdUsv.x3SNwhny', 'seller'),
+('Corentin','Villiermet','coco@wanadoo.fr','$2y$10$ZPbU3HJ5gQ3/HFh1xWXBYuybb6ZEXulAzm9Cww/5cK88WdNiBFdom', 'seller'),
+('Mickaël','Gaillard','micky@lycoos.com','$2y$10$7UILhfy1byfq8rSdnnXkuOxfB.29Y/yKtHgc5DH9D4M3iy5Ccr41K', 'admin');
 
-INSERT INTO `contact` (`firstName`, `lastName`, `phoneNumber`, `email`) VALUES
-("Ernst","Stavro Blofeld","01-01-01-76-50","blofeld_ernst@spectre.com"),
-("Julius","No","01-01-02-67-54","no_julius@spectre.com"),
-("Giovanni","Sakaki","+265 6 87 55 76 99","g.sakaki@team-rocket.net"),
-("Jessie","Musashi","+265 6 87 44 76 88","j.musashi@team-rocket.net"),
-("Ozwell","Spencer","1-651-435-265","ozwellspencer@umbrellacorp.com"),
-("Edward","Ashford","1-651-435-265","edwardashford@umbrellacorp.com");
+INSERT INTO `contact` (`first_name`, `last_name`, `phone_number`, `email`) VALUES
+('Ernst','Stavro Blofeld','01-01-01-76-50','blofeld_ernst@spectre.com'),
+('Julius','No','01-01-02-67-54','no_julius@spectre.com'),
+('Giovanni','Sakaki','+265 6 87 55 76 99','g.sakaki@team-rocket.net'),
+('Jessie','Musashi','+265 6 87 44 76 88','j.musashi@team-rocket.net'),
+('Ozwell','Spencer','1-651-435-265','ozwellspencer@umbrellacorp.com'),
+('Edward','Ashford','1-651-435-265','edwardashford@umbrellacorp.com');
 
-INSERT INTO `organization` (`companyName`, `streetAdress1`, `streetAdress2`, `streetAdress3`, `cityName`, `postalCode`, `country`, `phoneNumber`, `email`) VALUES
-("SPECTRE", "136 Boulevard Haussmann", null , null , "Paris", "75008", "France", "01-01-01-666", "contact@spectre.com"),
-("Team Rocket", "Casino", "entrée derrière le poster" , "sous-sol" , "Céladopole", "211-0205", "Kanto", "+265 1 23 55 76 90", "info@team-rocket.net"),
-("Umbrella Corporation", "Le manoir", null , null , "Raccoon City", "55131", "USA", "1-651-435-265", "contact@umbrellacorp.com");
+INSERT INTO `organization` (`company_name`, `street_address1`, `street_address2`, `street_address3`, `city_name`, `postal_code`, `country`, `phone_number`, `email`) VALUES
+('SPECTRE', '136 Boulevard Haussmann', null , null , 'Paris', '75008', 'France', '01-01-01-666', 'contact@spectre.com'),
+('Team Rocket', 'Casino', 'entrée derrière le poster' , 'sous-sol' , 'Céladopole', '211-0205', 'Kanto', '+265 1 23 55 76 90', 'info@team-rocket.net'),
+('Umbrella Corporation', 'Le manoir', null , null , 'Raccoon City', '55131', 'USA', '1-651-435-265', 'contact@umbrellacorp.com');
 
 
 
-INSERT INTO `product` (`name`, `reference`, `description`, `unitPriceBeforeTax`, `taxRate`, `quantityAvailable`, `isSellable`) VALUES
-("Magali", "MAG-NET-001", "Spécialisations : Journalistes, Politiques Techniques : armes à feu, poison, &#34accidents&#34 Localisation : France & International", 20000.00, 0.20, 1, true),
-("Mike", "MIK-NET-002", "Spécialisations : Particuliers Techniques : armes à feu, poison, &#34accidents&#34 Localisation : France & International", 10000.00, 0.15, 1, true),
-("Linda", "LIN-NET-003", "Spécialisations : Journalistes, Politiques Techniques : armes à feu, poison, &#34accidents&#34 Localisation : France & International", 10000.00, 0.10, 1, true),
-("Keylogger", "KEY-LOG-004", "Envie de savoir ce que votre belle-mère tape sur ses appareils ?", 50.76, 0.18, 6, true),
-("Spyware", "SPY-LOG-005", "Lancez-vous dans la collecte d'informations des institutions les plus prestigieuses de votre pays", 87.31, 0.15, 23, true),
-("Botnet software", "LIN-LOG-006", "Transformez des ordinateurs et serveurs peu sécurisés en une armée prête à se lancer à l'assaut de votre concurrence !", 99.99, 0.10, 46, true),
-("Acide fluorhydrique", "ACI-MAT-007", "Un voisin ennuyeux ?", 20000.00, 0.20, 1, true),
-("Kit de perçage", "PER-MAT-008", "Pour répondre aux questions que vous vous poserez une fois devant le coffre de la banque", 749.00, 0.20, 1, true),
-("Char d'assaut", "CHA-MAT-009", "Pour mener votre propre guerre... rien de personnel ? Si !", 2530000.00, 0.20, 1, true);
+INSERT INTO `product` (`name`, `reference`, `description`, `unit_price_before_tax`, `tax_rate`, `quantity_available`, `is_sellable`) VALUES
+('Magali', 'MAG-NET-001', 'Spécialisations : Journalistes, Politiques Techniques : armes à feu, poison, &#34accidents&#34 Localisation : France & International', 20000.00, 0.20, 1, true),
+('Mike', 'MIK-NET-002', 'Spécialisations : Particuliers Techniques : armes à feu, poison, &#34accidents&#34 Localisation : France & International', 10000.00, 0.15, 1, true),
+('Linda', 'LIN-NET-003', 'Spécialisations : Journalistes, Politiques Techniques : armes à feu, poison, &#34accidents&#34 Localisation : France & International', 10000.00, 0.10, 1, true),
+('Keylogger', 'KEY-LOG-004', 'Envie de savoir ce que votre belle-mère tape sur ses appareils ?', 50.76, 0.18, 6, true),
+('Spyware', 'SPY-LOG-005', 'Lancez-vous dans la collecte d''informations des institutions les plus prestigieuses de votre pays', 87.31, 0.15, 23, true),
+('Botnet software', 'LIN-LOG-006', 'Transformez des ordinateurs et serveurs peu sécurisés en une armée prête à se lancer à l''assaut de votre concurrence !', 99.99, 0.10, 46, true),
+('Acide fluorhydrique', 'ACI-MAT-007', 'Un voisin ennuyeux ?', 20000.00, 0.20, 1, true),
+('Kit de perçage', 'PER-MAT-008', 'Pour répondre aux questions que vous vous poserez une fois devant le coffre de la banque', 749.00, 0.20, 1, true),
+('Char d''assaut', 'CHA-MAT-009', 'Pour mener votre propre guerre... rien de personnel ? Si !', 2530000.00, 0.20, 1, true);
 
 INSERT INTO `category` (`name`) VALUES
-("Nettoyage"),
-("Logiciels"),
-("Matériel");
+('Nettoyage'),
+('Logiciels'),
+('Matériel');
 
 
 #datetime au format aaaammjj hh:mm:ss ex: '20120618 10:34:09 AM'
-INSERT INTO `purchase` (`reference`, `dateOfOrder`, `comment`) VALUES
-("2021C0001",'20210401',null),
-("2021C0002",'20210402',null),
-("2021C0003",'20210403',null),
-("2021C0004",'20210404',"attendre le 17 mars avant la livraison"),
-("2021C0005",'20210405',null),
-("2021C0006",'20210406',null),
-("2021C0007",'20210407',null),
-("2021C0008",'20210408',null),
-("2021C0009",'20210409',null);
+INSERT INTO `purchase` (`reference`, `date_of_order`, `comment`) VALUES
+('2021C0001','20210401',null),
+('2021C0002','20210402',null),
+('2021C0003','20210403',null),
+('2021C0004','20210404','attendre le 17 mars avant la livraison'),
+('2021C0005','20210405',null),
+('2021C0006','20210406',null),
+('2021C0007','20210407',null),
+('2021C0008','20210408',null),
+('2021C0009','20210409',null);
 
 INSERT INTO `product_in_purchase` (`quantity`, `price`) VALUES
 (1, 23.76),
