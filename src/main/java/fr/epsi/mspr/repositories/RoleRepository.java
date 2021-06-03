789d5b07ece42db39ffd8d939610c90739dab271
cvillier <corentin.villiermet@gmail.com>
2021-06-03 15:30:48 +0200

    create role entity and repository

---
+++ src/main/java/fr/epsi/mspr/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package fr.epsi.mspr.repositories;
+
+import fr.epsi.mspr.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
