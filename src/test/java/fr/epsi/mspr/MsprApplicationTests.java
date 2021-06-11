a7be701cccc7a61b0967a2438043f55ff59f6eb8
cvillier <corentin.villiermet@gmail.com>
2021-06-11 13:53:37 +0200

    final changes

--- src/test/java/fr/epsi/mspr/MsprApplicationTests.java
+++ src/test/java/fr/epsi/mspr/MsprApplicationTests.java
@@ -1,11 +1,13 @@
 package fr.epsi.mspr;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsprApplicationTests {
 
+	@Disabled
 	@Test
 	void contextLoads() {
 	}
